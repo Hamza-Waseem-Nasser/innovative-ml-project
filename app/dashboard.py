f4ff1e7a6032173df0053564b8a9b8da705cddea
Hamza Nasser <hamzawaseem.n@gmail.com>
2025-04-05 12:36:23 +0300

    Set up project structure and environment

---
+++ app/dashboard.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("🚀 Hamza’s First Streamlit Dashboard!")
+st.write("Everything is working and you're a pro now 😎")
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+
+print("🚀 Everything is working perfectly!")
